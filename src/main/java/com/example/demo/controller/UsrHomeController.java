2a5a1c33a21ec9be2c4ecfa661d34abe9dadfd69
Leeeydia <leeeydia@gmail.com>
2026-01-21 14:39:10 +0900

    lombok 오류 수정

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -1,36 +1,35 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Controller
 public class UsrHomeController {
-	
-	@RequestMapping("/usr/home/getDouble")
+
+	@RequestMapping("/usr/home/getArticle")
 	@ResponseBody
-	public double getDouble() {
-		return 3.14;
-	}
-	
-	
-	@RequestMapping("/usr/home/getBoolean")
-	@ResponseBody
-	public Boolean getBoolean() {
-		return true;
-	}
-	
-	
-	@RequestMapping("/usr/home/getString")
-	@ResponseBody
-	public String getString() {
-		return "abc";
+	public Article getArticle() {
+		Article article = new Article(1, "제목 1", "내용1");
+		return article;
 	}
 
-	@RequestMapping("/usr/home/getInt")
-	@ResponseBody
-	public int getInt() {
-		return 100;
-	}
+}
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Article{
+	int id;
+	String title;
+	String body;
 }
