720758fd15083b8bc5c1463465574f15a3b7eaa0
Leeeydia <leeeydia@gmail.com>
2026-01-21 14:06:17 +0900

    여러가지 리턴 타입

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -1,37 +1,36 @@
-
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
-	int count;
-
-	public UsrHomeController() {
-		count = 0;
+	
+	@RequestMapping("/usr/home/getDouble")
+	@ResponseBody
+	public double getDouble() {
+		return 3.14;
+	}
+	
+	
+	@RequestMapping("/usr/home/getBoolean")
+	@ResponseBody
+	public Boolean getBoolean() {
+		return true;
+	}
+	
+	
+	@RequestMapping("/usr/home/getString")
+	@ResponseBody
+	public String getString() {
+		return "abc";
 	}
 
-	@RequestMapping("/usr/home/setCountValue")
+	@RequestMapping("/usr/home/getInt")
 	@ResponseBody
-	public String setCountValue(int value) {
-		this.count = value;
-		return "count 값" + value + "(으)로 초기화";
-	}
-
-	@RequestMapping("/usr/home/setCount")
-	@ResponseBody
-	public String setCount_zero() {
-		count = 0;
-		return "count 값 0으로 초기화";
-	}
-
-	@RequestMapping("/usr/home/getCount")
-	@ResponseBody
-	public int showMain4() {
-		return count++;
+	public int getInt() {
+		return 100;
 	}
 
 }
