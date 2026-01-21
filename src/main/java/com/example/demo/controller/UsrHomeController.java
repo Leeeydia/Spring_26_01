7476c141fadd23afce7c5f50662815d4ae74a6fe
Leeeydia <leeeydia@gmail.com>
2026-01-21 11:19:17 +0900

    실행환경 세팅 수정

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -1,3 +1,4 @@
+
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
@@ -6,12 +7,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
-
-	@RequestMapping("/usr/home/main")
-	@ResponseBody
-
-	public String ShowMain() {
-		return "안녕";
-	}
-
+@RequestMapping("/usr/home/main")
+@ResponseBody
+public String showMain() {
+	return "안녕";
+}
 }
