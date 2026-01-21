d861178fc94b3b1ba7714e126a768cb2ef43c803
Leeeydia <leeeydia@gmail.com>
2026-01-21 12:17:09 +0900

    MVC, count 변수 컨트롤

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -3,13 +3,35 @@ package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UsrHomeController {
-@RequestMapping("/usr/home/main")
-@ResponseBody
-public String showMain() {
-	return "안녕";
-}
+	int count;
+
+	public UsrHomeController() {
+		count = 0;
+	}
+
+	@RequestMapping("/usr/home/setCountValue")
+	@ResponseBody
+	public String setCountValue(int value) {
+		this.count = value;
+		return "count 값" + value + "(으)로 초기화";
+	}
+
+	@RequestMapping("/usr/home/setCount")
+	@ResponseBody
+	public String setCount_zero() {
+		count = 0;
+		return "count 값 0으로 초기화";
+	}
+
+	@RequestMapping("/usr/home/getCount")
+	@ResponseBody
+	public int showMain4() {
+		return count++;
+	}
+
 }
